7dcc62c4bbc84f333eea3e336415435f07388e51
Mayur7862 <83858995+Mayur7862@users.noreply.github.com>
2024-07-15 22:38:17 +0530

    trying

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,30 @@ import {
 } from '@clerk/nextjs'
 
 import React from 'react'
+import { Inter, Space_Grotesk} from 'next/font/google'
+import type { Metadata } from 'next'
+import './globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500','600','700' ,'800', '900'],
+  variable: '--font-inter'
+})
+
+const SpaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500','600','700'],
+  variable: '--font-SpaceGrotesk'
+})
+
+
+export const metadata: Metadata = {
+  title: 'Devflow',
+  description: 'Kuch bhi poch le jo tuze pucna hai coding ke regarding',
+  icons:{
+    icon: './favicon.ico'
+  }
+}
 
 export default function RootLayout({
   children,
@@ -15,15 +39,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      elements: {
+        formButtonPrimary: 'primary-gradient',
+        footerActionLink: 
+        'primary-text-gradient hover:text-primary-500'
+      }
+    }
+    }>
       <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+        <body className={ `${inter.variable} ${SpaceGrotesk.variable}`}>
           {children}
         </body>
       </html>
